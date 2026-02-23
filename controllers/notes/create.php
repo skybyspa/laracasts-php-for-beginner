cfd24f8c02935aa3d45b61eaf80e0c035f6447a6
AprilEncountered <smah0070@student.monash.edu>
2026-02-23 15:28:33 +1100

    Namespacing and handle multiple request methods

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,5 +1,8 @@
 <?php
 
+use Core\Database;
+use Core\Validator;
+
 $config = require base_path('config.php');
 //connect to MySQL database
 $db = new Database($config['database']);
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -8,6 +8,11 @@
             <a class="text-blue-200 underline" href="/websites/demo/notes">Go Back</a>
         </p>
         <p class="text-white"> <?= htmlspecialchars($note['body']) ?></p>
+
+        <form class="mt-6" method="POST">
+            <input type="hidden" name="id" value=" <?= $note['id'] ?> ">
+            <button class="text-sm text-red-300">Delete</button>
+        </form>
     </div>
 </main>
 
